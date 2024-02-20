902dc7d76252e0f5d94e350d6b36d6be1766c03a
amypo-reactapp <reactapp@amypo.in>
2024-02-20 15:20:00 +0000

    User

---
+++ src/Image/Image.js
@@ -0,0 +1,9 @@
+import REACTAPP from 'react'
+const Image=()=>{
+    return(
+        <div>
+             <img src='https://res.cloudinary.com/practicaldev/image/fetch/s--qo_Wp38Z--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/e0nl7ziy1la7bpwj7rsp.png' height='100px' width='100px'/>
+        </div>
+    )
+}
+export default Image
---
+++ src/Reacts/Reacts.js
@@ -0,0 +1,16 @@
+import REACTAPP from 'react';
+
+const Reacts=()=>{
+    return(
+        <div>
+            <h6>React Skill Session</h6>
+            <p>React is a JavaScript library created by Facebook.</p>
+
+            <p>React is a User Interface (UI) library.</p>
+
+            <p>React is a tool for building UI components.</p>
+           
+        </div>
+    )
+}
+export default Reacts;
